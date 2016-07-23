f4695f38a87a7966da5ddd06dc94c5df16af16ea
pointsqatar <pointsqatar@outlook.com>
2016-07-23 09:57:06 +0300

    Metadata tags updated

---
+++ SpecializedCalibration/SpecializedCalibration/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SpecializedCalibration
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/js/bootstrap/min.js",
+            //            "~/js/custom.js",
+            //            "~/js/hover.js"
+            //            ));
+
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
+        }
+    }
+}
